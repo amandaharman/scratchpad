e9c4b99c317f28b550880f6763abe2dc8e69076c
Amanda Harman <amanda.harman@willowtreeapps.com>
2021-03-24 20:29:11 -0400

    Adds Xcode Project

---
+++ ScratchPad/ScratchPad/ScratchPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScratchPadApp.swift
+//  ScratchPad
+//
+//  Created by Amanda Harman on 3/24/21.
+//
+
+import SwiftUI
+
+@main
+struct ScratchPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
