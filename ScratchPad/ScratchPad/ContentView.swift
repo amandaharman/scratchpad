3f0c48a746ba30bf3a11e41743da8ffc4c818192
Amanda Harman <amanda.harman@willowtreeapps.com>
2021-03-24 21:07:16 -0400

    Adds a conversation starter

--- ScratchPad/ScratchPad/ContentView.swift
+++ ScratchPad/ScratchPad/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, Mandy!")
+        Text("Hello, Mandy! What's up?")
             .padding()
             .foregroundColor(.red)
     }
