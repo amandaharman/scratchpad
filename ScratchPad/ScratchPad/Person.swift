f8041995ae4d53c114a6963f09db4ccf3d2d4663
Amanda Harman <amanda.harman@willowtreeapps.com>
2021-03-26 14:09:38 -0400

    Makes changes

--- ScratchPad/ScratchPad/Person.swift
+++ ScratchPad/ScratchPad/Person.swift
@@ -11,5 +11,12 @@ struct Person {
     var name: String
     var age: Int
     var address: String
+    
+    static func printThings() {
+        print("1")
+        print("2")
+        print("3")
+    }
+    
 }
 
